8d341a3327e083075c73b08fe5730361e8605d9e
Héctor Pardo <kektoreon.0679@gmx.com>
2023-10-03 09:04:07 -0500

    Add files via upload

---
+++ AddSource.ps1
@@ -0,0 +1,6 @@
+$OPath = Get-Location
+$ConfigFile = "$($OPath)\ConfigOffice.xml"
+$InstallPath = "<Add OfficeClientEdition=`"64`" Channel=`"PerpetualVL2021`" SourcePath=`"$OPath`"`>"
+(Get-Content $ConfigFile) -replace '<Add.*', $InstallPath | Set-Content $ConfigFile
+$RunSetup = "$($OPath)\setup.exe"
+& $RunSetup /configure $ConfigFile
---
+++ RunSetup.cmd
@@ -0,0 +1,3 @@
+:start setup
+start powershell.exe -ExecutionPolicy Bypass -File AddSource.ps1
+goto End
